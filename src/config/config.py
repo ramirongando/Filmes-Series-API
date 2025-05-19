a53f2b70191dc1d44c7f7af7ff848c1b3cd394d1
ramirongando <rmendonca096@gmail.com>
2025-05-19 15:28:49 +0100

    Url para extrair as series

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,9 @@
 URL_BASE = "https://comandoplay.com"
 URL_MOVIES = f"{URL_BASE}/category/movies-2" # page/2/
 
+U_SERIES = "https://assistir.biz"
+URL_SERIES = f"{U_SERIES}/series-todas"
+
 HEADERS = {
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
     }
